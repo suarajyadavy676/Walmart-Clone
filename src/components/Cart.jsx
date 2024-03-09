df57bca0e4dec4826c85bd0602771f3e10ce18e4
Suraj Yadav <surajyadavy676@gmail.com>
2024-03-09 19:50:43 +0530

    add empty cart function

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,18 +1,19 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ContextData } from "../context/ContextProvider";
 import { useToast } from "@chakra-ui/react";
+import { Navigate } from "react-router-dom";
 
 function Cart() {
-  const toast = useToast()
-  const { cartData,setCartData } = useContext(ContextData);
-  const [remove,setRemove] = useState({})
+  const toast = useToast();
+  const { cartData, setCartData } = useContext(ContextData);
+  const [remove, setRemove] = useState({});
   // console.log(remove.id)
   // console.log(remove)
-  useEffect(()=>{
-    let cartRemove = cartData.filter((ele => ele.id != remove.id))
-    setCartData(cartRemove)
-  },[remove])
-  
+  useEffect(() => {
+    let cartRemove = cartData.filter((ele) => ele.id != remove.id);
+    setCartData(cartRemove);
+  }, [remove]);
+
   // console.log(cartData);
   return (
     <div className="p-4 max-w-xl mx-auto mt-32">
@@ -34,7 +35,10 @@ function Cart() {
                   <h2 className="text-lg font-bold">{ele.title}</h2>
                   <span className="text-gray-600">{`$ ${ele.price}`}</span>
                 </div>
-                <button onClick={()=>setRemove(ele)} className="text-gray-600 hover:text-red-500">
+                <button
+                  onClick={() => setRemove(ele)}
+                  className="text-gray-600 hover:text-red-500"
+                >
                   <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
                     <path d="M19 13H5v-2h14v2z" />
                   </svg>
@@ -96,17 +100,26 @@ function Cart() {
         <div className="px-4 py-3 bg-gray-200">
           <div className="flex justify-between items-center">
             <span className="font-bold text-lg">Total:</span>
-            <span className="font-bold text-lg">{`$ ${cartData?.reduce((total, currentValue) => total = total + currentValue.price,0)}`}</span>
+            <span className="font-bold text-lg">{`$ ${cartData?.reduce(
+              (total, currentValue) => (total = total + currentValue.price),
+              0
+            )}`}</span>
           </div>
-          <button onClick={() =>
-        toast({
-          title: 'Hooray! Your order was confirmed',
-          description: "Thank you for your order!",
-          status: 'success',
-          duration: 9000,
-          isClosable: true,
-        })
-      } className="block w-full mt-4 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
+          <button
+            onClick={() =>{
+              toast({
+                title: "Hooray! Your order was confirmed",
+                description: "Thank you for your order!",
+                status: "success",
+                duration: 9000,
+                isClosable: true,
+              })
+              setCartData([])
+            }
+              
+            }
+            className="block w-full mt-4 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
+          >
             Checkout
           </button>
         </div>
